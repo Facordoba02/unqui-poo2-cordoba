18e5bb43018850478c8697e75319e6d2e25fad10
Facundo Cordoba <Facordoba02@gmail.com>
2022-04-17 20:32:44 -0300

    multiplos con caso -1

--- src/ar/edu/unq/po2/tp3/Multiplos.java
+++ src/ar/edu/unq/po2/tp3/Multiplos.java
@@ -4,7 +4,7 @@ public class Multiplos {
 	
 	public static int multiploMasAltoEntre(int x, int y){
 		int multiplo = -1;
-		for(int i = 0; i <= 1000; i++){
+		for(int i = 1; i <= 1000; i++){
 			if(esMultiploDe(i, x, y)) {
 				multiplo = i;
 			}
