63c1ab8de6e43ed30b29ed8e73938f4c81e2d80c
Facundo Cordoba <Facordoba02@gmail.com>
2022-04-17 17:50:25 -0300

    transformo los metodos a static

--- src/ar/edu/unq/po2/tp3/EvenDigits.java
+++ src/ar/edu/unq/po2/tp3/EvenDigits.java
@@ -1,30 +1,30 @@
 package ar.edu.unq.po2.tp3;
 
 public class EvenDigits {
-	private int[] numbers;
-
-	public void setNumbers(int[] numbers) {
-		this.numbers = numbers;
-	}
+//	private int[] numbers;
+//
+//	public void setNumbers(int[] numbers) {
+//		this.numbers = numbers;
+//	}
 	
-	public int counterEvenDigits() {
-		int max = this.evenCount(numbers[0]); 
+	public static int counterEvenDigits(int[] numbers) {
+		int max = evenCount(numbers[0]); 
 		int result = 0;
 		
 		for(int i = 1; i < numbers.length; i++) {
-			if(this.evenCount(numbers[i]) > max) {
-				max = this.evenCount(numbers[i]);
+			if(evenCount(numbers[i]) > max) {
+				max = evenCount(numbers[i]);
 				result = numbers[i];
 			}
 		}
 		return result;
 	}
 	
-	public int evenCount(int num) {
+	public static int evenCount(int num) {
 		int number = num;
 		int counter = 0;
 		while(number > 0) {
-			if((number % 10) % 2 == 0) {
+			if(number % 2 == 0) {
 				counter += 1;
 			}
 			number /= 10;
--- tests/ar/edu/unq/po2/tp3/test/EvenDigitsTest.java
+++ tests/ar/edu/unq/po2/tp3/test/EvenDigitsTest.java
@@ -1,32 +1,32 @@
 package ar.edu.unq.po2.tp3.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ar.edu.unq.po2.tp3.EvenDigits;
 
 class EvenDigitsTest {
-	private EvenDigits evenDigits;
-	
-	@BeforeEach
-	public void setUp() throws Exception{
-		evenDigits = new EvenDigits();
-		int[ ] numberList = {451, 23, 114, 2461, 225, 461, 12};
-		evenDigits.setNumbers(numberList);
-	}
+//	private EvenDigits evenDigits;
+//	
+//	@BeforeEach
+//	public void setUp() throws Exception{
+//		evenDigits = new EvenDigits();
+//		int[] numberList = {451, 23, 114, 2461, 225, 461, 12};
+//		evenDigits.setNumbers(numberList);
+//	}
 	
 	@Test
 	public void testEvenCount() {
-		int result = evenDigits.evenCount(1234);
+		int result = EvenDigits.evenCount(1234);
 		assertEquals(result, 2);
 	}
 	
 	@Test
 	public void testEvenCounterDigits() {
-		int result = evenDigits.counterEvenDigits();
-		assertEquals(result, 2461);
+		int[] numberList = {451, 23, 11422, 2461, 225, 461, 12};
+		int result = EvenDigits.counterEvenDigits(numberList);
+		assertEquals(result, 11422);
 	}
 
 
