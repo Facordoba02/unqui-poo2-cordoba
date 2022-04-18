5e54da0f3aa8818e39bf5ed68423780e38fcb20a
Facundo Cordoba <Facordoba02@gmail.com>
2022-04-18 07:39:01 -0300

    Punto 5

---
+++ src/ar/edu/unq/po2/tp3/Punto5.java
@@ -0,0 +1,10 @@
+package ar.edu.unq.po2.tp3;
+
+public class Punto5 {
+	/**
+	 * 1) vienen predefinidos en Java y no representan a un objeto.
+	 * 2) int es un tipo primitivo e integer es una clase.
+	 * 3) int inicializa en 0, cuando integer se inicializa en null.
+	 * 4) int pide que sea inicializada para poder ser utilizada
+	 * */
+}
