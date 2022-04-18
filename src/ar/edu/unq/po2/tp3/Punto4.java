e213ab9b4a7c495a83c1cae9bf56882aef453719
Facundo Cordoba <Facordoba02@gmail.com>
2022-04-17 21:06:20 -0300

    Punto 4

---
+++ src/ar/edu/unq/po2/tp3/Punto4.java
@@ -0,0 +1,25 @@
+package ar.edu.unq.po2.tp3;
+
+public class Punto4 {
+
+	/**
+	 * Dado el siguiente código:
+		String a = "abc";
+		String s = a;
+		String t;
+		Indique que valores retornan las siguientes expresiones o, si dan error, por qué se producen:
+		-> s.length(); = 3
+		-> t.length(); = null;
+		-> 1 + a; = "1abc"
+		-> a.toUpperCase(); = "A"
+		-> "Libertad".indexOf("r"); = 4
+		-> "Universidad".lastIndexOf('i'); = 7
+		-> "Quilmes".substring(2,4); = "ilm"
+		-> (a.length() + a).startsWith("a"); = false
+		-> s == a; = true
+		-> a.substring(1,3).equals("bc") = true
+	*/
+	
+	
+	
+}
